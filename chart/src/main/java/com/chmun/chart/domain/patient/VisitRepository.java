cd88b8b1d136651536c2c4dbdc80a214f93389e4
Cheolhyun Mun <zooxop@gmail.com>
2024-03-05 22:26:36 +0900

    [Add] JPARepository - Patient, Visit

---
+++ chart/src/main/java/com/chmun/chart/domain/patient/VisitRepository.java
@@ -0,0 +1,7 @@
+package com.chmun.chart.domain.patient;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VisitRepository extends JpaRepository<Visit, Long> {
+
+}
